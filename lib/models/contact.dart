47ff866cf5e4fab983b7d1573a6639ae6a2bfd3c
mukaramatu <iddrisumukaramatu0@gmail.com>
2021-12-22 12:44:13 -0800

    first commit

---
+++ lib/models/contact.dart
@@ -0,0 +1,16 @@
+class Contact {
+  String name = "";
+  String number = "";
+  String id = "";
+  String email = "";
+  String proFileImage = "";
+
+  Contact(
+    {this.name = "",
+    required this.number,
+    this.email = "",
+    this.proFileImage = "",
+    required this.id
+    }
+  );
+}
